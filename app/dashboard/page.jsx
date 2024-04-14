7cc01d188cc120d42862a126fd99acb3aa699fdd
Hrithik Bagane <hrithiknanasaheb.bagane@sjsu.edu>
2024-04-14 13:59:40 -0700

    auth with mongodb, next-auth, neurelo done

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import Info from '@/components/Info'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <Info />
+    </div>
+  )
+}
+
+export default Dashboard
